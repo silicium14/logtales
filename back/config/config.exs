39d3d1c65136acc2ac6f9303eeea0ae6c8145f05
silicium14 <13800015+silicium14@users.noreply.github.com>
2017-09-07 07:07:25 +0200

    Use different mnesia directories per mix environment

--- back/config/config.exs
+++ back/config/config.exs
@@ -5,10 +5,12 @@ config :logger, backends: [:console]
 # file: the path of the log file
 # regex: a regex used for parsing and filtering log events.
 #   It must have the three named capture groups date, item and content.
+# date_format: the Timex format to use to parse dates captured by the `regex`.
 config :back,
   file: "../tmp/events.log",
   regex: ~r/^\[(?<date>\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d)\] \[(?<item>[^\]]+)\] (?<content>.*)/,
   date_format: "{YYYY}-{0M}-{0D} {h24}:{m}:{s}"
 
 config :mnesia,
-  dump_log_write_threshold: 100000
+  dump_log_write_threshold: 100000,
+  dir: 'mnesia/#{Mix.env}'
