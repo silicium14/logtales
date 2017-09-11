052b97fb329f76aa9f2bfcac94bd3b0a13711fa8
silicium14 <13800015+silicium14@users.noreply.github.com>
2017-09-11 08:01:57 +0200

    Add mnesia test "the events are filtered by range"

--- back/test/mnesia_test.exs
+++ back/test/mnesia_test.exs
@@ -24,4 +24,30 @@ defmodule MnesiaTest do
     ### Assert
     assert result == [event]
   end
+
+  test "the events are filtered by range" do
+    ### Arrange
+    Mnesia.resetdb
+    event_in_range = %{
+      date: DateTime.from_naive!(~N[2017-01-01 00:00:01], "Etc/UTC"),
+      item: "an item",
+      content: "The content"
+    }
+    event_out_of_range = %{
+      date: DateTime.from_naive!(~N[2017-01-02 00:00:01], "Etc/UTC"),
+      item: "an item",
+      content: "The content"
+    }
+    # Load into Mnesia
+    Mnesia.load Flow.from_enumerable([{0, event_in_range}, {1, event_out_of_range}])
+
+    ### Act
+    result = Mnesia.events(
+      DateTime.from_naive!(~N[2017-01-01 00:00:00], "Etc/UTC"),
+      DateTime.from_naive!(~N[2017-01-01 10:00:00], "Etc/UTC")
+    )
+
+    ### Assert
+    assert result == [event_in_range]
+  end
 end
