0ee50c5bdfbf104c9884032249aaaa8c1e9c7ef8
silicium14 <13800015+silicium14@users.noreply.github.com>
2017-09-12 07:32:33 +0200

    Update README.md, add the bin/load.bash script

--- bin/demo.bash
+++ bin/demo.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# Fetches backend dependences
 # Resets the database, loads the configured log file
 # and starts the application
+
 cd back
 mix deps.get
-mix load
 cd ..
+bin/load.bash
 bin/run.bash
---
+++ bin/load.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Resets the database, loads the configured log file
+
+cd back
+mix load
+cd ..
