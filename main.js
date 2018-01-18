8c7999a02d3f772caaf5dbb34cc4444748f375ef
silicium14 <13800015+silicium14@users.noreply.github.com>
2018-01-18 20:05:34 +0100

    Let electron app kill backend on exit

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 let { app, BrowserWindow } = require('electron')
+let { spawn, spawnSync } = require('child_process')
 
 let mainWindow = null
 let devMode = process.env.NODE_ENV === 'development'
+
+let command = 'mix'
+let args = ['run' , '--no-halt']
+
+if (process.platform === 'win32') {
+  // garsh durn windows-nodejs and its inability to spawn a non-exe
+  args.unshift('/d', '/s', '/c', command)
+  command = process.env.comspec || 'cmd.exe'
+}
+
+let backend = spawn(command, args, { cwd: __dirname })
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('quit', () => {
+  // windows needs to run `taskkill` since it actually spawns cmd and tells that to spawn iex
+  // it also needs to run it synchronously so it actually kills before electron quits
+  if (process.platform === 'win32') spawnSync('taskkill', ['/pid', backend.pid, '/f', '/t'])
+  else backend.kill('SIGKILL')
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
