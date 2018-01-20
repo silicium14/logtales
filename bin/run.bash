4971f94ab4532537f32e33358647471f2c4e2845
silicium14 <13800015+silicium14@users.noreply.github.com>
2018-01-20 13:58:20 +0100

    Modify run script to only run electron app

--- bin/run.bash
+++ bin/run.bash
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-# Compiles the elm frontend and starts the backend server (that serves the frontend)
-
-set -e
-
-cd front
-echo "- Building front-end"
-elm-make Main.elm --yes --warn --output app.html
-echo "- Finished building front-end"
-cd ..
-
 yarn start
